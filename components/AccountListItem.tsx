68f414ede4c75a7df5b633052e2b66ef0684cde0
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-02-22 16:34:30 +0530

    Add: New account UI

--- components/AccountListItem.tsx
+++ components/AccountListItem.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function AccountListItem() {
   return (
-    <View>
-      <Text>Profit</Text>
+    <View style={styles.container}>
+      <Text style={styles.name}>Profit</Text>
+      <Text style={styles.percentage}>10%</Text>
+      <Text style={styles.percentage}>20%</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    // borderWidth: 2,
+    // borderStyle: "solid",
+    // borderColor: "black",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderRadius: 5,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  percentage: {},
+})
