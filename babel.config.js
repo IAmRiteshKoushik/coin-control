215c3be4c5e3527d8bb8776251b0d729f199a063
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-02-27 01:48:12 +0530

    Init WatermelonDB, custom dev build

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-export default function (api) {
+export default function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"]
+    presets: ["babel-preset-expo"],
+    plugins: ['@babel/plugin-proposal-decorators', { legacy: true }],
   };
 };
